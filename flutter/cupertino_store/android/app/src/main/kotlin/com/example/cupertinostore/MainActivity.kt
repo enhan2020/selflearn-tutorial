fea83c3786e29b2773c565fc5cb003a480ee4f65
Your Name <you@example.com>
2020-04-28 17:23:30 +0800

    refactor repository

--- flutter/cupertino_store/android/app/src/main/kotlin/com/example/cupertinostore/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.cupertinostore
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutter/friendlychat/android/app/src/main/kotlin/com/example/friendlychat/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.friendlychat
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
